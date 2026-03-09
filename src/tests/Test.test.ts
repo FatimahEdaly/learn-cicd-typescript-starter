b50b0eff5a0cce42865509fa09b8e478af79873d
FatimahEdaly <jalalfatimah977@gmail.com>
2026-03-09 21:12:03 +0200

    failing3

--- src/tests/Test.test.ts
+++ src/tests/Test.test.ts
@@ -1,23 +1,14 @@
 import { describe, expect, test } from "vitest";
-import { getAPIKey } from "../api/auth"; // استيراد الدالة الحقيقية من المشروع
 
-describe("getAPIKey", () => {
-  test("returns the correct API key from headers", () => {
-    // محاكاة للهيدرز التي يرسلها المستخدم
-    const headers = {
-      authorization: "ApiKey hello-world-123"
-    };
-    
-    const result = getAPIKey(headers);
-    
-    // نتوقع أن الدالة تستخلص "hello-world-123" فقط
-    expect(result).toBe("hello-world-123");
-  });
+const person = {
+  isActive: false,
+  age: 32,
+};
 
-  test("throws an error if Authorization header is missing", () => {
-    const headers = {}; // هيدرز فارغة
-    
-    // نتوقع أن الدالة ستفشل (Throw Error)
-    expect(() => getAPIKey(headers)).toThrow();
+describe("person", () => {
+  
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
   });
 });
